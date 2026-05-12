5ae932a3f27570947cb68dc5f786b132d483dce5
Varun Shakya <varunshakya1794@gmail.com>
2026-05-12 09:32:16 +0530

    fixed page names

--- pages/5_admin.py
+++ pages/5_admin.py
@@ -1,7 +1,5 @@
 import streamlit as st
 from db import get_client, login, logout, fixtures_group, update_after_res
-from utils.styles import inject_styles
-inject_styles()
 
 st.set_page_config(page_title="Admin", page_icon="static/logo.png", layout="wide")
 st.logo("static/logo.png")
