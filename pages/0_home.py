8f25f5296e342f07ec0cc6a09af04f16eb63472f
Varun Shakya <varunshakya1794@gmail.com>
2026-05-12 13:49:36 +0530

    Refactor font loading and HTML styling for header

--- pages/0_home.py
+++ pages/0_home.py
@@ -1,26 +1,6 @@
 import streamlit as st
 import streamlit.components.v1 as components
-from utils.styles import tusker_title
-
 import base64
-
-def load_font(path):
-    with open(path, "rb") as f:
-        return base64.b64encode(f.read()).decode()
-
-t800 = load_font("static/fonts/TuskerGrotesk-8700Bold.woff2")
-
-st.markdown(f"""
-<style>
-@font-face {{
-    font-family: 'Tusker Grotesk';
-    src: url('data:font/woff2;base64,{t800}') format('woff2');
-    font-weight: 800;
-    font-display: swap;
-}}
-h1 {{ font-family: 'Tusker Grotesk', sans-serif !important; font-weight: 800 !important; }}
-</style>
-""", unsafe_allow_html=True)
 
 st.set_page_config(
     page_title="FIFA WC Predictor",
@@ -33,24 +13,25 @@ if 'scheduler_started' not in st.session_state:
     start()
     st.session_state.scheduler_started = True
 
-components.html("""
+with open("static/fonts/TuskerGrotesk-8700Bold.woff2", "rb") as f:
+    t800 = base64.b64encode(f.read()).decode()
+
+components.html(f"""
 <style>
-@font-face {
+@font-face {{
     font-family: 'Tusker Grotesk';
-    src: url('/app/static/fonts/TuskerGrotesk-8700Bold.woff2') format('woff2');
+    src: url('data:font/woff2;base64,{t800}') format('woff2');
     font-weight: 800;
-}
-h1 {
+}}
+h1 {{
     text-align: center;
     font-family: 'Tusker Grotesk', sans-serif;
     font-weight: 800;
     font-size: 5rem;
     color: white;
     margin: 0;
-}
+    line-height: 1.1;
+}}
 </style>
-<h1><br>
-    WELCOME TO MY<br>
-    2026 FIFA WORLD CUP<br>
-    PREDICTOR</h1>
+<h1><br>WELCOME TO MY<br>2026 FIFA WORLD CUP<br>PREDICTOR</h1>
 """, height=600)
